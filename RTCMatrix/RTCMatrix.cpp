24f58992e4cb78de4bb3d160bf905c4008c22573
Jeffrey Goldsmith <phoenixofstone@yahoo.ca>
2016-03-13 00:59:26 -0500

    The 12th Hour
    Added special case for 12 AM and 12 PM. In these cases the clock now
    displays 12 instead of 0.

--- RTCMatrix/RTCMatrix.cpp
+++ RTCMatrix/RTCMatrix.cpp
@@ -140,7 +140,7 @@ void Display::DisplayTime()
   bitTime(tm.mon, bitLength[5], row[5]);
   bitTime(tm.d, bitLength[4], row[4]);
   bitTime(tm.wd, bitLength[3], row[3]);
-  bitTime(tm.h % 12, bitLength[2], row[2]);
+  tm.h == 12 ? bitTime(12, bitLength[2], row[2]) : bitTime(tm.h % 12, bitLength[2], row[2]);
   bitTime(tm.m, bitLength[1], row[1]);
   bitTime(tm.s, bitLength[0], row[0]);
 
@@ -148,7 +148,9 @@ void Display::DisplayTime()
   {
     for (byte i = 0; i < ROW_OFFSET; i++) // Set APM row to Green
       matrix.setPixelColor(ROW_OFFSET * APM_ROW - i - 1, 0, COLOUR, 0);
-  } else { // If not AM assume PM
+  } 
+  else 
+  { // If not AM assume PM
     for (byte i = 0; i < ROW_OFFSET; i++) // Set APM row to Blue
       matrix.setPixelColor(ROW_OFFSET * APM_ROW - i - 1, 0, 0, COLOUR);
   }
@@ -161,10 +163,12 @@ void Display::DisplayTime()
       isDay = false;
       isNight = true;
     }
-  } else { // If not assume time is between 8AM and 8PM
+  } 
+  else 
+  { // If not assume time is between 8AM and 8PM
     if (isNight == true)
     {
-      matrix.setBrightness(1); // Set brightness to night mode
+      matrix.setBrightness(10); // Set brightness to night mode
       isDay = true;
       isNight = false;
     }
